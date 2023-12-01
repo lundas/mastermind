df68c227d3e27ce9cb8202389ed3469096e0cb96
Andrew Lund <andrew.s.lund@gmail.com>
2023-11-30 18:06:02 -0800

    added get and post requests to App and GuessInput

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import GuessInput from './GuessInput';
 import GuessHistoryList from './GuessHistoryList'
 import GuessCounter from './GuessCounter'
 
 export default function App() {
+  let [gameStarted, setGameStarted] = useState(false);
+  let [gameId, setGameId] = useState(null);
+  let [guessList, setGuessList] = useState([]);
+
+  function startGame() {
+    return axios.get('/api');
+  }
+
+  function makeGuess(gameId, guess) {
+    return axios.post('/api', { gameId, guess });
+  }
+
   return (
     <>
       <h1>MasterMind</h1>
-      <GuessInput />
-      <GuessHistoryList />
-      <GuessCounter />
+      { gameStarted ? (
+        <>
+          <GuessInput
+            gameId={gameId}
+            makeGuess={makeGuess}
+            guessList={guessList}
+            setGuessList={setGuessList}
+          />
+          <GuessHistoryList />
+          <GuessCounter />
+        </>
+      ) : (
+        <>
+          <button type="button" onClick={(e) => {
+            e.preventDefault();
+            setGameStarted(true);
+            startGame()
+              .then((result) => {setGameId(result.data.gameId)})
+              .catch((err) => console.error('startGame error: ', err));
+          }}>Start Game</button>
+        </>
+      )
+    }
     </>
   )
 }
--- client/src/components/GuessInput.jsx
+++ client/src/components/GuessInput.jsx
@@ -1,50 +1,39 @@
 import React from 'react';
 
-export default function GuessInput() {
+function GuessSelect({ name }) {
   return (
+    <select name={name}>
+        <option value="0">0</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+      </select>
+  )
+}
+
+export default function GuessInput({
+  gameId, makeGuess, guessList, setGuessList
+}) {
+  return(
     <div id="guess-input-container">
       <h3>Make Your Guess</h3>
-      <select name="guess1">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-      </select>
-      <select name="guess2">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-      </select>
-      <select name="guess3">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-      </select>
-      <select name="guess4">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-      </select>
-      <button type="button" onClick={(e) => console.log("Submit Button Clicked")}>Submit</button>
+      <GuessSelect name={'guess1'}/>
+      <GuessSelect name={'guess2'}/>
+      <GuessSelect name={'guess3'}/>
+      <GuessSelect name={'guess4'}/>
+      <button type="button" onClick={(e) => {
+        e.preventDefault();
+        let selectNodes = document.querySelectorAll('select');
+        let guess = '';
+        selectNodes.forEach((s) => guess += s.value);
+        makeGuess(gameId, guess)
+          .then((result) => setGuessList([...guessList, result.data]))
+          .catch((err) => console.error('makeGuess error: ', err));
+      }}>Submit</button>
     </div>
   )
 }
