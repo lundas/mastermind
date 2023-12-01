225fd3f2fbddeb8d4c4b27655f367cdb1a2bbe1a
Andrew Lund <andrew.s.lund@gmail.com>
2023-11-30 18:05:28 -0800

    added morgan for server logging

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const { initializeGame, makeGuess } = require('./controllers')
 
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/api', initializeGame);
 
